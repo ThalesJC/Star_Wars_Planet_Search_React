e829dc0ca37149d13fd9780655a1fe3f6ec7673d
ThalesJC <thales.soul96@gmail.com>
2022-07-19 21:42:04 -0300

    Commit Inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <Provider>
+      <span>Hello, App!</span>
+    </Provider>
   );
 }
 
